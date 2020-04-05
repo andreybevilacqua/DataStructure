2ccd9d124b6e733081a82479b7f0bbc18d00bd3a
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2020-04-05 16:23:13 +0200

    Adds List example

---
+++ src/main/java/exercises/ListExamples.java
@@ -0,0 +1,11 @@
+package exercises;
+
+import java.util.List;
+
+public class ListExamples {
+
+  public static void main(String... args) {
+    List<String> names = List.of("A", "B");
+    System.out.println(names);
+  }
+}
