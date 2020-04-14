279059eb212c672e35487cd0a10a66b7715a2a7d
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2020-04-14 18:47:12 +0200

    Adds ConcurrentHashMap example

---
+++ src/main/java/exercises/ConcurrentHashMapExample.java
@@ -0,0 +1,9 @@
+package exercises;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapExample {
+  public static void main(String[] args) {
+    ConcurrentHashMap a = new ConcurrentHashMap();
+  }
+}
